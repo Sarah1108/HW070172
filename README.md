3e99795e22710f67b1ad73150f063001fba89bf5
Sarah1108 <sarah.sulollari@gmail.com>
2020-10-20 20:45:13 +0200

    I think I understood.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # HW070172
 This is my homework
+
+And i finally managed to copy and open it from my laptop. 
---
+++ hw_2/exercise_7.py
@@ -0,0 +1,12 @@
+# exercise_7
+
+def main() :
+    print("This program illustrates a chaotic function")
+    x = eval(input("Enter a number between 0 and 1: "))
+    y = eval(input("Enter a number between 0 and 1: "))
+    for i in range(10) :
+        x = 3.9 * x * (1-x)
+        y = 3.9 * y * (1-y)
+        print("     %.6f,       %.6f" % (x, y))
+
+main()
