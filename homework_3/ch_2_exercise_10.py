b1599e11b3e55ba08955cf1f34ca1062cf2e2c98
Sarah1108 <sarah.sulollari@gmail.com>
2020-11-24 08:23:15 +0100

    Memex

--- homework_3/ch_2_exercise_10.py
+++
@@ -1,8 +0,0 @@
-# A program to convert distances in kilometers
-# to miles.
-
-def main():
-    km = eval(input("Enter the distance in kilometers:  "))
-    miles = km * 0.62
-    print("The distance in miles is ", miles)
-main()
--- homework_3/ch_2_exercise_9.py
+++
@@ -1,9 +0,0 @@
-#convert.py
-#   A program to convert Celisus temos to Fahrenheit.
-# By Susan Computewell (Phyton Book p.30)
-
-def main():
-    fahrenheit = eval(input("What is the Fahrenheit temerature? "))
-    celsius = (fahrenheit-32)/ (9/5)
-    print("The temperature is", celsius, "degrees Celsius")
-main()
